59505f10fcecccd141e36fb69db722a0e862aa78
rfpoulos <rfpoulos@outlook.com>
2018-07-06 11:50:20 -0400

    sign in now uses local state to update sign in form

--- src/views/sign-in/sign-in.js
+++ src/views/sign-in/sign-in.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './style.css';
 import helpers from './helpers';
 import { connect } from 'react-redux';
-import { compose } from 'recompose';
+import { compose, withState, withHandlers } from 'recompose';
 import TextInput from '../../components/text-input/text-input';
 
-let SignInDumb = () =>
+let SignInDumb = ({ signInForm, handleEmail, handlePassword }) =>
     <div className="sign-in">
-        <TextInput type="email" placeholder="email"/>
-        <TextInput type="password" placeholder="password" />
+        <TextInput type="text" 
+            placeholder="email or username"
+            value={ signInForm.identifier }
+            onChange={ handleEmail }/>
+        <TextInput type="password"
+            placeholder="password"
+            value={ signInForm.password }
+            onChange={ handlePassword }/>
     </div>
 
-export default SignInDumb;
+let SignIn = compose(
+    withState('signInForm', 'updateInput', { identifier: '', password: '' }),
+    withHandlers({
+        handleEmail: ({signInForm, updateInput}) => event => 
+            updateInput({...signInForm, identifier: event.target.value}),
+        handlePassword: ({signInForm, updateInput}) => event =>
+            updateInput({...signInForm, password: event.target.value})
+    })
+)(SignInDumb)
+
+export default SignIn;
