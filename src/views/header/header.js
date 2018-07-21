bb21503cf445d00ec5d2a7ecac7f94b208dcea5d
rfpoulos <rfpoulos@outlook.com>
2018-07-20 23:16:50 -0400

    minor styling

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './style.css';
 import Logo from '../../components/logo/logo'
-import Menu from '../../components/menu/menu';
 
-export let Header = ({ 
-    updateMenuOpen, 
-    menuOpen, 
-    userObject,
-    handleToggle,
-}) =>
+export let Header = () =>
     <header className="header">
         <Logo />
     </header>
